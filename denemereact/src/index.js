3d399abaa2ac040c4a39710f500c3922d73af85c
Emirhan <63867996+emirhanincodes@users.noreply.github.com>
2021-05-25 21:56:04 +0300

    new lesson

---
+++ denemereact/src/index.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+
+const App = () => {  
+    return(
+        <div>Hi There</div>
+    )
+}
+
+ReactDOM.render(
+    <App />,
+    document.querySelector('#root')
+)
