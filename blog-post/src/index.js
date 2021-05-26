7ec40b967dee7c3c9a41e444b26718610b6b7977
Emirhan <63867996+emirhanincodes@users.noreply.github.com>
2021-05-26 13:48:45 +0300

    react :squirrel:

---
+++ blog-post/src/index.js
@@ -0,0 +1,15 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+const App = () => {
+    return(
+        <div>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Suscipit laudantium minus libero repudiandae quod nesciunt debitis illo dicta saepe beatae, in odio doloremque incidunt tenetur est vero et quibusdam sit.
+        </div>
+
+    )
+}
+ReactDOM.render(
+    <App />,
+    document.querySelector('#root')
+)
