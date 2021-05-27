3fb8c0a45147986af1f54d8c3ecc06dd5e67fa5f
Emirhan <63867996+emirhanincodes@users.noreply.github.com>
2021-05-27 16:32:28 +0300

    react :love:

--- location-destination/src/HemisphereDisplay.jsx
+++ location-destination/src/HemisphereDisplay.jsx
@@ -1,10 +1,33 @@
-import React from 'react';
+import React from "react";
+import Northern from "./img/n.png";
+import Southern from "./img/s.png";
+import "./hemisphere.css";
 
-const HemisphereDisplay = () => {
-    return(
-        <div className="hemis">
-            Hey mne i
+const hemisphereConfig = {
+  Northern: {
+    text: 'burası kuzey yarımküre',
+    picture: Northern,
+  },
+  Southern: {
+    text: 'burası güney yarımküre',
+    picture: Southern,
+  },
+};
+
+const HemisphereDisplay = ({ latitude }) => {
+  const hemisphere = latitude > 0 ? 'Northern' :  'Southern';
+  const { text, picture } = hemisphereConfig[hemisphere];
+
+  return (
+    <div className={hemisphere}>
+      <div className="ui raised text container segment">
+        <div className="image">
+          <img src={picture} alt="Loksyon" />
         </div>
-    )
-}
-export default HemisphereDisplay
+        <div className="ui teal bottom attached label"></div>
+        <h1> Lokasyon : {text} </h1>
+      </div>
+    </div>
+  );
+};
+export default HemisphereDisplay;
--- location-destination/src/index.jsx
+++ location-destination/src/index.jsx
@@ -1,33 +1,30 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
+import React from "react";
+import ReactDOM from "react-dom";
+import HemisphereDisplay from "./HemisphereDisplay";
 
 class App extends React.Component {
-    constructor(props) {
-        super(props);
+  
+  state = { latitude:null,errorMessage:''}
 
-        this.state = {latitude: null};
-        window.navigator.geolocation.getCurrentPosition(
-            (position) => {
-                this.setState({latitude: position.coords.latitude})
-            },
-            (error) => {
-                this.setState({errorMessage: error.message})
-            }
-            );
+  componentDidMount() {
+    window.navigator.geolocation.getCurrentPosition(
+      (position) => {
+        this.setState({ latitude: position.coords.latitude });
+      },
+      (error) => {
+        this.setState({ errorMessage: error.message });
+      }
+    );
+  }
+  render() {
+    if (this.state.errorMessage && !this.state.latitude) {
+      return <div> {this.state.errorMessage} </div>;
     }
-    render() {
-        
-        if (this.state.errorMessage && !this.state.latitude) {
-            return <div> {this.state.errorMessage} </div>
-        }
-        if (this.state.errorMessage && !this.state.latitude) {
-            return <div> {this.state.latitude} </div>
-        }else{
-            return <div>loading</div>
-        }
+    if (!this.state.errorMessage && this.state.latitude) {
+      return <div> <HemisphereDisplay latitude={this.state.latitude} /> </div>;
+    } else {
+      return <div>loading</div>;
     }
+  }
 }
-ReactDOM.render(
-    <App />,
-    document.querySelector('#root')
-)
+ReactDOM.render(<App />, document.querySelector("#root"));
