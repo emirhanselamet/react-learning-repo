8fe2ebccd18cf432b048c71d870d731562084352
Emirhan <63867996+emirhanincodes@users.noreply.github.com>
2021-05-28 11:48:44 +0300

    :sunflower:

--- image-finder/src/component/PhotoList.jsx
+++ image-finder/src/component/PhotoList.jsx
@@ -1,11 +1,11 @@
 import React from "react";
-
+import './images.css'
 const PhotoList = (props) => {
     const images = props.images.map((image) =>{
         return <img key={image.id} src={image.webformatURL} alt="image" />
     })
     return(
-    <div>
+    <div className='list'>
         {images}
     </div>
     )
