7aedcfc9eaa0ef1c347da10ecba157871ccf1fd1
Emirhan <63867996+emirhanincodes@users.noreply.github.com>
2021-06-05 20:06:48 +0300

    Update Card.jsx

--- react-router/src/components/Card.jsx
+++ react-router/src/components/Card.jsx
@@ -1,20 +1,30 @@
-import React from 'react';
+import React from "react";
+import { connect } from "react-redux";
 
 class Card extends React.Component {
-    state = {user:''}
-    componentDidMount() {
-        let user = this.props.match.params.user
-        this.setState({user})
-    }
-    render() {
-        const {user} = this.state
-        return(
-            <div className='ui raised very padded text container segment'
-        style={{marginTop:'80px'}}>
-            <h3 className='ui header'>{user}</h3>
-            
-        </div>
-        )
-    }
+//   state = { user: "" };
+//   componentDidMount() {
+//     let user = this.props.match.params.user;
+//     this.setState({ user });
+//   }
+  render() {
+   const {title,body} =this.props.card;
+    return (
+      <div
+        className="ui raised very padded text container segment"
+        style={{ marginTop: "80px" }}
+      >
+        <h3 className="ui header">{title}</h3>
+        <p>{body}</p>
+      </div>
+    );
+  }
 }
-export default Card;
+const mapStateToProps = (state, ownProps) => {
+  let title = ownProps.match.params.user;
+
+  return {
+    card: state.cards.find((card) => card.title === title),
+  };
+};
+export default connect(mapStateToProps)(Card);
